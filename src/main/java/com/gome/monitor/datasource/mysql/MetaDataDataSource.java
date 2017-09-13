407959d1fe92ca36e2d8a3f6481f58f4a1139725
lujiali <253503945@qq.com>
2017-09-13 10:38:09 +0800

    lujiali9月13号修改jdbc 连接中断

--- src/main/java/com/gome/monitor/datasource/mysql/MetaDataDataSource.java
+++ src/main/java/com/gome/monitor/datasource/mysql/MetaDataDataSource.java
@@ -18,6 +18,18 @@ public class MetaDataDataSource {
     private String url;
     private String username;
     private String password;
+    private int maxActive;
+    private int maxIdle;
+    private int minIdle;
+    private int initialSize;
+    private int maxWait;
+    private int removeAbandonedTimeout;
+    private boolean removeAbandoned;
+    private boolean testOnBorrow;
+    private boolean testOnReturn;
+    private boolean testWhileIdle;
+    private String validationQuery;
+    private int timeBetweenEvictionRunsMillis;
 
     @Bean(name = "metaDataSource")
     public DataSource metaDataDataSource(){
@@ -26,6 +38,19 @@ public class MetaDataDataSource {
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
+        dataSource.setInitialSize(initialSize); // 连接池启动时创建的初始化连接数量（默认值为0）
+        dataSource.setMaxActive(maxActive); // 连接池中可同时连接的最大的连接数
+        dataSource.setMaxIdle(maxIdle); // 连接池中最大的空闲的连接数，超过的空闲连接将被释放，如果设置为负数表示不限
+        dataSource.setMinIdle(minIdle); // 连接池中最小的空闲的连接数，低于这个数量会被创建新的连接
+        dataSource.setMaxWait(maxWait); // 最大等待时间，当没有可用连接时，连接池等待连接释放的最大时间，超过该时间限制会抛出异常，如果设置-1表示无限等待
+        dataSource.setRemoveAbandonedTimeout(removeAbandonedTimeout); // 超过时间限制，回收没有用(废弃)的连接
+        dataSource.setRemoveAbandoned(removeAbandoned); // 超过removeAbandonedTimeout时间后，是否进 行没用连接（废弃）的回收
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis); // 检查无效连接的时间间隔 设为30分钟
+
         return dataSource;
     }
 }
--- src/test/java/com/gome/monitor/MonitorApplicationTests.java
+++ src/test/java/com/gome/monitor/MonitorApplicationTests.java
@@ -8,26 +8,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MonitorApplicationTests {
 
-	@Autowired
-	ScheduledTasks scheduledTasks;
-	@Autowired
-	MysqlmonitorService mysqlmonitorService;
-	@Test
-	public void contextLoads() {
+    @Autowired
+    ScheduledTasks scheduledTasks;
+    @Autowired
+    MysqlmonitorService mysqlmonitorService;
+
+    @Test
+    public void contextLoads() {
 //		scheduledTasks.test2();
-	}
+    }
 
-	@Test
-	public void test(){
-		mysqlmonitorService.mysqlmonitorstate();
-	}
+    @Test
+    public void test() {
+        mysqlmonitorService.mysqlmonitorstate();
+    }
 
-	@Test
-	public void test2(){
-		scheduledTasks.checkmaindata();
-	}
+    @Test
+    public void test2() {
+        List<Map<String, Object>> mysqlmonitorlogs = mysqlmonitorService.mysqlmonitorlogs();
+        System.out.print(mysqlmonitorlogs);
+    }
 }
